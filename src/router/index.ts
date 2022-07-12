18962a81f6ace91755d111ee3e8ddac213fd7ef1
Dmitrii <doronin_dima@mail.ru>
2022-07-12 20:17:10 +0300

    routes

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import React from "react";
+import Login from "../pages/Login";
+import Event from "../pages/Event";
+
+
+export interface IRoute {
+  path: string;
+  element: any;
+}
+
+export enum RouteNames {
+  LOGIN = '/login',
+  EVENT = '/event'
+}
+
+export const publicRoutes: IRoute[] = [
+  {path: RouteNames.LOGIN, element: Login}
+];
+
+export const privateRoutes: IRoute[] = [
+  {path: RouteNames.EVENT, element: Event}
+];
