391830a82338782d72d909e5e607b02e0fb23f1a
DoroninDmitrii <doronin_dima@mail.ru>
2022-07-23 14:12:04 +0300

    devtools type

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,18 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore, compose } from "redux";
 import thunk from "redux-thunk";
 import reducer from './sumReducers'
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers(reducer);
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
